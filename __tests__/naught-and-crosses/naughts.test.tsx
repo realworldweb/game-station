73458f35f14e3b872b9dccb35dd4187e92456701
realworldred <paulrooney60@gmail.com>
2023-03-24 11:55:45 +0000

    added test for has won

--- __tests__/naught-and-crosses/naughts.test.tsx
+++ __tests__/naught-and-crosses/naughts.test.tsx
@@ -1,6 +1,14 @@
+/*react testing libary*/
 import { render, screen, fireEvent } from '@testing-library/react';
+
+/*component*/
 import Naughts from '@/pages/naughts-and-crosses';
+
+/*layouts*/
 import Layout from '../../layouts/main';
+
+/*utils*/
+import { hasWon, computerTurn } from '@/lib/helpers/naughts';
 
 describe('naughts', () => {
 	it('renders naughts unchanged', () => {
@@ -100,5 +108,47 @@ describe('naughts', () => {
 		fireEvent.click(tiles[0]);
 
 		expect(playerDetails).toHaveTextContent('Player: player2');
+	});
+
+	describe('has won', () => {
+		it('should return true if winning row', () => {
+			const won = hasWon(['x', 'x', 'x', 'o', 'o', '', '', '', '']);
+
+			expect(won).toBe(true);
+		});
+
+		it('should return true if winning column', () => {
+			const won = hasWon(['x', 'o', 'x', 'x', 'o', '', 'x', '', '']);
+
+			expect(won).toBe(true);
+		});
+
+		it('should return true if winning diagonal', () => {
+			const won = hasWon(['x', 'o', 'x', 'o', 'x', '', '', '', 'x']);
+
+			expect(won).toBe(true);
+		});
+
+		it('should return false if not winning', () => {
+			const won = hasWon(['x', 'o', 'x', 'o', 'x', '', '', 'x', '']);
+
+			expect(won).toBe(false);
+		});
+	});
+
+	describe('computerTurn', () => {
+		it('should add a filled tile to the board', () => {
+			const board = ['x', '', '', '', '', '', '', '', ''];
+			const tookTurn = computerTurn(board, 'o');
+
+			expect(tookTurn).toContain('o');
+		});
+
+		it('should choose middle square if available', () => {
+			const board = ['x', '', '', '', '', '', '', '', ''];
+			const tookTurn = computerTurn(board, 'o');
+
+			expect(tookTurn[4]).toContain('o');
+		});
 	});
 });
